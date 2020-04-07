4b86963115de93dc0172f5ff48644b6735622361
饶家俊 <raojiajun@bimwinner.com>
2020-04-07 22:48:46 +0800

    优化 「区分开发环境与生产环境, 主要是生产环境排除 react 库」

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {test: /\.tsx?$/, loader: 'ts-loader'}
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [new HtmlWebpackPlugin({  // Also generate a test.html
+    filename: 'index.html',
+    template: './src/index.html'
+  })],
+  externals: {
+    'react': 'react', // Case matters here
+    'react-dom': 'reactDOM' // Case matters here
+  }
+};
